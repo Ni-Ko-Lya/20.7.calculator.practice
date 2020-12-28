c4ad596a47df516c0fb635a84e30db0702a4931d
Ni-Ko-Lya <65465767+Ni-Ko-Lya@users.noreply.github.com>
2020-12-29 02:15:33 +0300

    initial commit of the calculator app

---
+++ 20.7.Practice.Calculator/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  20.7.Practice.Calculator
+//
+//  Created by Николай Павленко on 28.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var displayCalc: UILabel!
+    @IBOutlet weak var button0: UIButton!
+    @IBOutlet weak var buttonDot: UIButton!
+    @IBOutlet weak var buttonEqual: UIButton!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var buttonPlus: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var buttonMinus: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    @IBOutlet weak var buttonMultiply: UIButton!
+    @IBOutlet weak var buttonDivide: UIButton!
+    @IBOutlet weak var buttonAC: UIButton!
+    @IBOutlet weak var buttonPlusMinusSign: UIButton!
+    @IBOutlet weak var buttonPercent: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
